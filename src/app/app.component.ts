f0a80dc291e9573c08c4cf9f1e6b29b68857d4e3
unknown <kevin.tang@gmx.ch>
2021-03-09 23:43:56 +0100

    latest version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
-import { InteractiveGraph,GraphType, InteractiveGraphService } from 'interactive-graph'
+import { Component, OnChanges, OnInit } from '@angular/core';
+import { InteractiveGraph, GraphType, InteractiveGraphService } from 'interactive-graph'
+import { Router, RouterLink, RouterLinkActive } from '@angular/router'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnChanges {
+  router: Router;
+
+  constructor(router: Router) {
+    this.router = router;
+  }
+
+
+  ngOnChanges() {
+    console.log("CHANNNGE")
+  }
+
+  customLink: boolean = false;
+
+  currentLink() {
+    this.customLink = !this.customLink
+  }
+
+
 }
